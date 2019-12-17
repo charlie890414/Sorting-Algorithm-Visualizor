89963df7a2459eca6c5cd0dc95b7adbf6d564c98
scott0002 <scott890719@gmail.com>
2019-12-17 19:28:54 +0800

    Finish RandomGenerator & Text

--- windbg/RandomGenerator.asm
+++ windbg/RandomGenerator.asm
@@ -4,7 +4,17 @@ RandomGenerator PROC,
     array: PTR DWORD,
     length: DWORD
 pushad
- 
+mov ecx,length-1
+.WHILE ecx>=0
+    dec ecx
+    call Randomize
+    mov eax,ecx+1
+    call RandomRange
+    mov ebx, array[ecx]
+    mov edx, array[eax]
+    mov array[ecx], edx
+    mov array[eax], rbx
+.ENDW
 popad
 ret
 RandomGenerator ENDP
