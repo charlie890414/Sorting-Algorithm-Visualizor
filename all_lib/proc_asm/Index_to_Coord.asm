de0ef8243dab550c881015b92bc409237620e8f9
tsai1247 <55378870+tsai1247@users.noreply.github.com>
2019-12-18 14:26:56 +0800

    upload index to coord

---
+++ all_lib/proc_asm/Index_to_Coord.asm
@@ -0,0 +1,24 @@
+INCLUDE irvine32.inc
+
+.data
+
+.code
+Index_to_Coord PROC, 
+	basicPos: COORD, def: WORD, index: DWORD
+pushad
+	
+	mov eax, 2
+	add ax, def
+	mul index
+	add basicPos.x, ax
+
+popad
+
+	mov ax, basicPos.x
+	shl eax, 16
+	add ax, basicPos.y
+
+ret
+
+Index_to_Coord ENDP
+end
