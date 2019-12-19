563ceddf3312efc968d7a399c6a3c1f844080460
tsai1247 <55378870+tsai1247@users.noreply.github.com>
2019-12-19 13:59:00 +0800

    upload main

--- Main.asm
+++ Main.asm
@@ -1,22 +1,24 @@
 INCLUDE library54.inc
 ;constant block
 NumbersLength=50
-AlgorithmTypeNumber=1
+AlgorithmTotalNumber=1
 ;constant block
 
 .data
 consoleHandle DWORD ?
-xyPos COORD <?, ?>
-;windowSize RECT <0,0,99,24>
+xyPos COORD <200, 50>
+basicPos COORD <5, 15>
+
+windowSize SMALL_RECT <0,0, 199, 49>
 
 consoleTitle BYTE "Sorting Algorithm Visualizor", 0
 
 key WORD ? ;user input 
 
-Numbers DWORD NumbersLength dup(?)       ;
+Sequence DWORD NumbersLength dup(?)
 IsNumberSorted BYTE NumbersLength dup(0) ;A boolean value to indicate the number is sorted or not.
 IsAnimationStopped BYTE ? ;A boolean value to indicate the animation is stopped or not.
-AnimationSpeed DWORD ? ;A integer indicate speed of the animation, it should be bounded.
+AnimationSpeed DWORD ? ;A integer indicate speed of the animation, it should be bounded. [1%, 100%]
 AlgorithmState DWORD 1
 
 
@@ -25,18 +27,30 @@ main EQU start@0
 .code
 main PROC
 ;SETTING console attributes
-;INVOKE GETSTDHANDLE, STD_OUTPUT_HANDLE					; mov eax, handle 
-;INVOKE setConsoleCursorPosition, consoleHandle, xyPos	; mov cursor to xyPos
-;call ClrScr											; clear console
 INVOKE GETSTDHANDLE, STD_OUTPUT_HANDLE
 mov consoleHandle,eax
-INVOKE SetConsoleTitle, ADDR consoleTitle
-;INVOKE SetConsoleScreenBufferSize, consoleHandle, xyPos
-;INVOKE SetConsoleWindowInfo, consoleHandle, 0, OFFSET windowSize
-;----------------------------------------------------------------------------
-.WHILE 1
 
-.ENDW
+INVOKE SetConsoleTitle, ADDR consoleTitle
+
+INVOKE setCursorVisible, consoleHandle, 0
+INVOKE SetConsoleScreenBufferSize, consoleHandle, xyPos
+INVOKE SetConsoleWindowInfo, consoleHandle, 1, OFFSET windowSize
+;----------------------------------------------------------------------------
+
+START:
+	.Switch AlgorithmState
+	.Case 1
+		.break
+	.Case 2
+
+		.break
+	.Default
+
+		.break
+	.EndSwitch
+
+JMP START
+
 exit
 main ENDP
 END main
