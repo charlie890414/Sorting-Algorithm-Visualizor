9b8657bf0dc4171b840a5ea58a170b6c4dc3f1f6
scott0002 <scott890719@gmail.com>
2019-12-21 20:41:14 +0800

    Update

--- make_Any.bat
+++ make_Any.bat
@@ -1,28 +1,28 @@
 @echo off
 
-del keyEvents.lst
-del keyEvents.obj
-del keyEvents.ilk
-del keyEvents.pdb
-del keyEvents.exe
+del RandomGenerator.lst
+del RandomGenerator.obj
+del RandomGenerator.ilk
+del RandomGenerator.pdb
+del RandomGenerator.exe
 
 setlocal 
 set INCLUDE=./
 set LIB=./
 set PATH=./
 
-.\all_lib\ML /c /coff /Zi   keyEvents.asm
+.\all_lib\ML /c /coff /Zi   RandomGenerator.asm
 if errorlevel 1 goto terminate
 
-.\all_lib\LINK /INCREMENTAL:no /debug /subsystem:console /entry:start /out:keyEvents.exe keyEvents.obj .\all_lib\obj\*.obj .\all_lib\Irvine32.lib .\all_lib\Kernel32.lib .\all_lib\user32.lib
+.\all_lib\LINK /INCREMENTAL:no /debug /subsystem:console /entry:start /out:RandomGenerator.exe RandomGenerator.obj .\all_lib\obj\*.obj .\all_lib\Irvine32.lib .\all_lib\Kernel32.lib .\all_lib\user32.lib
 if errorlevel 1 goto terminate
 
-DIR keyEvents.*
+DIR RandomGenerator.*
 
 :terminate
 
-MOVE /Y keyEvents.obj .\all_lib\obj
-COPY /Y keyEvents.asm .\all_lib\proc_asm
+MOVE /Y RandomGenerator.obj .\all_lib\obj
+COPY /Y RandomGenerator.asm .\all_lib\proc_asm
 
 pause
 endlocal
