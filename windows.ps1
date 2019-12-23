ebbccdbb336e5bfdb4039185f552f7fc07b247a2
charlie890414 <charlie890414@gmail.com>
2019-12-23 21:43:13 +0800

    add certificate

--- windows.ps1
+++ windows.ps1
@@ -1,3 +1,3 @@
 $Password = ConvertTo-SecureString -String 'Travis' -AsPlainText -Force
 Import-PfxCertificate -FilePath certificate.pfx -CertStoreLocation Cert:\LocalMachine\My -Password $Password
-& "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\signtool.exe" sign -t http://timestamp.comodoca.com/authenticode -f certificate.pfx Sorting.Algorithm.Visualizor.exe -p $Password
+& "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\signtool.exe" sign -t http://timestamp.comodoca.com/authenticode -f certificate.pfx Sorting.Algorithm.Visualizor.exe -Password $Password
