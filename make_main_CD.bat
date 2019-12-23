cb6d9eff972dba329438df6afc592e7e77719a05
charlie890414 <charlie890414@gmail.com>
2019-12-23 11:58:42 +0800

    final demo

--- make_main_CD.bat
+++ make_main_CD.bat
@@ -4,7 +4,7 @@ del Main.lst
 del Main.obj
 del Main.ilk
 del Main.pdb
-del Main.exe
+del Sorting Algorithm Visualizor.exe
 
 setlocal 
 set INCLUDE=./
@@ -14,7 +14,7 @@ set PATH=./
 .\all_lib\ML /c /coff /Zi   Main.asm
 if errorlevel 1 goto terminate
 
-.\all_lib\LINK /INCREMENTAL:no /debug /subsystem:console /entry:start /out:Main.exe Main.obj .\all_lib\obj\*.obj .\all_lib\Irvine32.lib .\all_lib\Kernel32.lib .\all_lib\user32.lib
+.\all_lib\LINK /INCREMENTAL:no /debug /subsystem:console /entry:start /out:Sorting Algorithm Visualizor.exe Main.obj .\all_lib\obj\*.obj .\all_lib\Irvine32.lib .\all_lib\Kernel32.lib .\all_lib\user32.lib
 if errorlevel 1 goto terminate
 
 del Main.lst
