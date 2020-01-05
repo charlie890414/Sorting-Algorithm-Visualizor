3372546474641537a65e1730d90b64ff5b838b8c
charlie890414 <charlie890414@gmail.com>
2020-01-05 21:37:20 +0800

    fix help algo showing

--- certificate.bat
+++
@@ -1,3 +0,0 @@
-certutil -addstore -f "ROOT" server.crt
-
-pause
--- windows.ps1
+++ windows.ps1
@@ -2,4 +2,4 @@ $Password = ConvertTo-SecureString -String 'Travis' -AsPlainText -Force
 
 Import-PfxCertificate -FilePath certificate.pfx -CertStoreLocation Cert:\LocalMachine\My -Password $Password
 
-& "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\signtool.exe" sign -v -debug -fd sha256 -t http://timestamp.comodoca.com/authenticode -sm -s My -n `"localhost`" -d `"Desc`" Sorting.Algorithm.Visualizor.exe
+& "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\signtool.exe" sign -v -debug -fd sha256 -sm -s My -n `"charlie890414`" -d `"Desc`" Sorting.Algorithm.Visualizor.exe
