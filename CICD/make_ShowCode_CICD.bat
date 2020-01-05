67c2c77f4f567c921b385b3193e3c195a4d28108
charlie890414 <charlie890414@gmail.com>
2020-01-05 22:09:57 +0800

    MOVE

---
+++ CICD/make_ShowCode_CICD.bat
@@ -0,0 +1,28 @@
+@echo off
+
+del ShowCode.lst
+del ShowCode.obj
+del ShowCode.ilk
+del ShowCode.pdb
+del ShowCode.exe
+
+setlocal 
+set INCLUDE=./
+set LIB=./
+set PATH=./
+
+.\all_lib\ML /c /coff /Zi   ShowCode.asm
+if errorlevel 1 goto terminate
+
+.\all_lib\LINK /INCREMENTAL:no /debug /subsystem:console /entry:start /out:ShowCode.exe ShowCode.obj .\all_lib\obj\*.obj .\all_lib\Irvine32.lib .\all_lib\Kernel32.lib .\all_lib\user32.lib
+if errorlevel 1 goto terminate
+
+DIR ShowCode.*
+
+:terminate
+
+MOVE /Y ShowCode.obj .\all_lib\obj
+COPY /Y ShowCode.asm .\all_lib\proc_asm
+
+pause
+endlocal
--- CICD/windows.ps1
+++ CICD/windows.ps1
@@ -1,5 +1,5 @@
 $Password = ConvertTo-SecureString -String 'Travis' -AsPlainText -Force
 
-Import-PfxCertificate -FilePath certificate.pfx -CertStoreLocation Cert:\LocalMachine\My -Password $Password
+Import-PfxCertificate -FilePath CICD\certificate.pfx -CertStoreLocation Cert:\LocalMachine\My -Password $Password
 
 & "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\signtool.exe" sign -v -debug -fd sha256 -sm -s My -n `"charlie890414`" -d `"Desc`" Sorting.Algorithm.Visualizor.exe
